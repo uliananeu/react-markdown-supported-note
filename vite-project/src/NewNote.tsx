9a1b9f9dc03acfbfc379cae397ee7b3ec8bc9562
uliananeu <139770965+uliananeu@users.noreply.github.com>
2023-11-27 11:35:40 +0200

    add NewNote component with NoteForm integration

--- vite-project/src/NewNote.tsx
+++ vite-project/src/NewNote.tsx
@@ -1,3 +1,10 @@
+import { NoteForm } from "./NoteForm";
+
 export function NewNote() {
-  return <h1>Hi</h1>;
+  return (
+    <>
+      <h1 className="mb-4">New Note</h1>
+      <NoteForm />
+    </>
+  );
 }
