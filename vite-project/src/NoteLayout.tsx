1edd214fbbc148fa5879215d8e2083e4e5503bda
uliananeu <139770965+uliananeu@users.noreply.github.com>
2023-12-02 22:16:53 +0200

    create Note.tsx

--- vite-project/src/NoteLayout.tsx
+++ vite-project/src/NoteLayout.tsx
@@ -1,4 +1,9 @@
-import { Navigate, Outlet, useParams } from "react-router-dom";
+import {
+  Navigate,
+  Outlet,
+  useOutletContext,
+  useParams,
+} from "react-router-dom";
 import { Note } from "./App";
 
 type NoteLayoutProps = {
@@ -12,4 +17,8 @@ export function NoteLayout({ notes }: NoteLayoutProps) {
   if (note == null) return <Navigate to="/" replace />;
 
   return <Outlet context={note} />;
+}
+
+export function useNote() {
+  return useOutletContext<Note>();
 }
